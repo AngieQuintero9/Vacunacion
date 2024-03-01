980ae32423c1a3b2c660f3e597476106e2378c02
AngieQuintero9 <148457172+AngieQuintero9@users.noreply.github.com>
2024-03-01 18:01:02 -0500

    Add files via upload

---
+++ Negocio/INegocioMaestro.cs
@@ -0,0 +1,12 @@
+﻿using Entidades;
+
+namespace Negocio
+{
+    public interface INegocioMaestro
+    {
+        List<Departamento> ObtenerDepartamentos();
+        List<Municipio> ObtenerMunicipios(int idDepartamento);
+        List<RegimenAfiliacion> ObtenerRegimenAfiliacion();
+        List<TipoDocumento> ObtenerTiposDocumento();
+    }
+}
---
+++ Negocio/NegocioMaestro.cs
@@ -0,0 +1,35 @@
+﻿using Entidades;
+using Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class NegocioMaestro : INegocioMaestro
+    {
+        IRepositorioMaestro repositorioMaestro = new RepositorioMaestroEF();
+        public NegocioMaestro()
+        {
+
+        }
+        public NegocioMaestro(IRepositorioMaestro repositorioMaestro)
+        {
+            this.repositorioMaestro = repositorioMaestro;
+        }
+
+        public List<Municipio> ObtenerMunicipios(int idDepartamento)
+        => repositorioMaestro.ObtenerMunicipios(idDepartamento);
+
+        public List<Departamento> ObtenerDepartamentos()
+        => repositorioMaestro.ObtenerDepartamentos();
+
+        public List<TipoDocumento> ObtenerTiposDocumento()
+        => repositorioMaestro.ObtenerTiposDocumento();
+
+        public List<RegimenAfiliacion> ObtenerRegimenAfiliacion()
+        => repositorioMaestro.ObtenerRegimenAfiliacion();
+    }
+}
